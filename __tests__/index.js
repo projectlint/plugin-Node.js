7d8e2bc4c4e4d77c33128e9464a9c2705bf863d1
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2020-01-14 23:03:28 +0100

    Added smoke test

---
+++ __tests__/index.js
@@ -0,0 +1,26 @@
+test("smoke", function() {
+  const result = require("..");
+
+  expect(result).toMatchInlineSnapshot(`
+    Object {
+      "config": Object {
+        "recommended": Object {
+          "version": Object {
+            "error": Object {
+              "status": "maintained",
+            },
+            "warning": Object {
+              "status": "lts_active",
+            },
+          },
+        },
+      },
+      "rules": Object {
+        "version": Object {
+          "evaluate": [Function],
+          "fetch": [Function],
+        },
+      },
+    }
+  `);
+});
